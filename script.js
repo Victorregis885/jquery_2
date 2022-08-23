25fb186ec4ce26bf49ee7ed7fb2cd297aa9e6ba3
Victorregis885 <victorregis5002@gmail.com>
2022-08-23 08:48:16 -0300

    commit

---
+++ script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $('p').hide()
+
+    $('#btn-resumo').click(function(e){
+        e.preventDefault()
+
+
+        // Criar uma variavel para receber o status
+
+        let status = $(this).attr('data-status')
+
+        $('#btn-resumo').empty()
+
+        if(status == '1'){
+            $('#btn-resumo').append('Esconder resumo')
+            $('#btn-resumo').attr('data-status', '2')
+        }
+        else{
+            $('#btn-resumo').append('Ver resumo')
+            $('#btn-resumo').attr('data-status', '1')
+
+        }
+        $('p').toggle(2000)
+    })
+})
